d1bcc18e51909dd0579a61c477628f0b1d5ac30e
Srinivasan1716 <srinivasan2470047@ssn.edu.in>
2026-08-20 20:15:42 +0530

    Changes for running backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/user.route";
+import authRoutes from "./routes/auth.route";
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 export default app;
