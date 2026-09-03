99e1bf42d7bb83983c44b86c08f97550fb1ae0da
Srinivasan1716 <srinivasan2470047@ssn.edu.in>
2026-09-03 22:20:28 +0530

    Change in the file

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
           </h1>
 
           <p className="text-zinc-400 mt-2">
-            Track your coding progress and improve your knowledge
+            Track your coding progress and to improve 
           </p>
         </div>
 
