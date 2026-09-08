dea4654bbd0171a4fe543d9af209a39f6c896845
Srinivasan1716 <srinivasan2470047@ssn.edu.in>
2026-09-08 22:54:50 +0530

    Changes in frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
           </h1>
 
           <p className="text-zinc-400 mt-2">
-            Track your code and improve daily......
+            Track your code and improve ............
           </p>
         </div>
 
